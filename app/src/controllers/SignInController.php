76c6dba2723756a74a6859d2c1a492ba8c245ce5
seong0318 <seong0318@naver.com>
2020-02-06 16:36:59 +0900

    eslint와 prettier 설정해봄 잘되는지는 모르겠음.

--- app/src/controllers/SignInController.php
+++ app/src/controllers/SignInController.php
@@ -5,18 +5,25 @@ namespace App\Controller;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-final class SignInController extends BaseController
-{
-    public function signIn(Request $request, Response $response, $args)
-    {
-        $this->view->render($response, 'sign_in.twig', ['user_name' => $user_name, 'pwd' => $pwd, 'remember' => $remember]);
+final class SignInController extends BaseController {
+    public function getUsnAndHashedPwd($username) {
+        /*  사용자 이름으로 usn과 hashed_pwd를 받아옴
+        **  ['usn': value, 'hashed_pwd': value]
+        */
+        $sql = "select usn, hashed_pwd from user where user_name = :username";
+        $stmt = $this->em->getConnection()->prepare($sql);
+        $params = ['username' => $username];
+        if (!$stmt->execute($params)) return -1;
+        $execResult = $stmt->fetch();
+        return $execResult;
     }
 
-    public function signInHandle(Request $request, Response $response, $args)
-    {
-        print_r($_POST);
+    public function signIn(Request $request, Response $response, $args) {
+        $this->view->render($response, 'sign_in.twig');
+    }
 
-        // $this->view->render($response, 'index.twig');
-        // exit;
+    public function signInHandle(Request $request, Response $response, $args) {
+        $execResult = $this->getUsnAndHashedPwd($_POST['user_name']);
+        echo "success " . $execResult['usn'] . ' ' . $execResult['hashed_pwd'];
     }
 }
